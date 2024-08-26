bbaa77b8e287d60873e15640070f8380b2a325e9
GabrielJapa123 <ueoka.gabriel@escola.pr.gov.br>
2024-08-26 11:33:53 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,16 @@
+
+function setup() {
+createCanvas(400, 400);
+ //F=m*a
+
+function segundaLei(massa, aceleracao){
+   let forca=massa*aceleracao;  
+ console.log(forca);
+   return forca;
+}
+ segundaLei (10,20);
+}
+
+
+function draw() {    
+}
